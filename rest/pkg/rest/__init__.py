46f0ddd73c2ae1a3f19dabff851a1e090551367e
vodchella <twister.kz@gmail.com>
2020-01-14 18:42:03 +0600

    ValidationError handling instead of RequestValidationError

--- rest/pkg/rest/__init__.py
+++ rest/pkg/rest/__init__.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
 from pkg.config import CONFIG
 from pkg.constants.error_codes import ERROR_UNHANDLED_EXCEPTION, ERROR_CUSTOM_EXCEPTION
 from pkg.rest.routers.root import router as root_router
 from pkg.rest.routers.v1 import router as v1_router
 from pkg.utils.errors import CustomException, response_error
 from pkg.utils.logger import DEFAULT_LOGGER
+from pydantic import ValidationError
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
@@ -40,8 +40,8 @@ async def http_exceptions_handler(request: Request, exc: HTTPException):
     )
 
 
-@app.exception_handler(RequestValidationError)
-async def http_exceptions_handler(request: Request, exc: RequestValidationError):
+@app.exception_handler(ValidationError)
+async def http_exceptions_handler(request: Request, exc: ValidationError):
     return response_error(
         ERROR_UNHANDLED_EXCEPTION,
         str(exc),
