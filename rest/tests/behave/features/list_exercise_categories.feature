145b38007963a12616ec10b59a4b1ee95576c0d1
vodchella <twister.kz@gmail.com>
2020-01-17 12:28:43 +0600

    Tests for REST /exercise/list-categories

---
+++ rest/tests/behave/features/list_exercise_categories.feature
@@ -0,0 +1,5 @@
+Feature: List exercise categories
+
+  Scenario: Do it right
+      Given I try to get exercise categories right
+       Then I will get Ok http status
---
+++ rest/tests/behave/features/steps/list_exercise_categories.py
@@ -0,0 +1,9 @@
+from behave import *
+from behave.runner import Context
+from tests.behave.utils import behave_request
+
+
+@given('I try to get exercise categories right')
+def step_impl(context: Context):
+    url = 'http://localhost:8517/v1/exercise/list-categories'
+    context.response = behave_request('GET', url)
