114442be93e8baa3bf24274e79e854a05a1610cc
vodchella <twister.kz@gmail.com>
2020-02-05 19:03:20 +0600

    New REST /token/refresh

--- rest/pkg/rest/routers/v1/token.py
+++ rest/pkg/rest/routers/v1/token.py
@@ -9,9 +9,9 @@ router = APIRouter()
 auth_scheme = HTTPBearer()
 
 
-async def get_current_user(token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> User:
+async def _get_current_user(token: str, expected_token_type: str) -> User:
     error_message = 'Недействительный токен'
-    payload = JwtService.validate_token(token.credentials, 'a')
+    payload = JwtService.validate_token(token, expected_token_type)
     if payload:
         user = await UserService.get_by_id(payload.uid)
         if user:
@@ -25,6 +25,19 @@ async def get_current_user(token: HTTPAuthorizationCredentials = Depends(auth_sc
     raise HTTPException(HTTP_401_UNAUTHORIZED, error_message)
 
 
+async def get_current_user(token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> User:
+    return await _get_current_user(token.credentials, 'a')
+
+
+async def get_current_user_by_refresh_token(token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> User:
+    return await _get_current_user(token.credentials, 'r')
+
+
 @router.get('/check')
 async def check(current_user: User = Depends(get_current_user)):
     return current_user
+
+
+@router.get('/refresh')
+async def refresh(current_user: User = Depends(get_current_user_by_refresh_token)):
+    return await JwtService.create_token_pair(current_user.id)
