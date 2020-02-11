66f0b8c06604b0814006b84d097cca16939b7aba
vodchella <twister.kz@gmail.com>
2020-02-11 18:17:12 +0600

    Tests for /exercise/:id/view REST

--- rest/tests/behave/features/steps/view_exercise.py
+++ rest/tests/behave/features/steps/view_exercise.py
@@ -4,19 +4,19 @@ from tests.behave.utils import authorized_behave_request
 from tests.behave.utils.constants import SERVER_API_V1_PATH
 
 
-@given('I try to get exercise history right')
+@given('I try to view exercise right')
 def step_impl(context: Context):
-    url = f'{SERVER_API_V1_PATH}/exercise/mqwaAIQblczmUExp/history'
+    url = f'{SERVER_API_V1_PATH}/exercise/3j7VPlHrsw5vf9YR/view'
     context.response = authorized_behave_request('GET', url)
 
 
-@given('I try to specify invalid exercise ID for view history')
+@given('I try to specify invalid exercise ID')
 def step_impl(context: Context):
-    url = f'{SERVER_API_V1_PATH}/exercise/some-invalid-id/history'
+    url = f'{SERVER_API_V1_PATH}/exercise/some-invalid-id/view'
     context.response = authorized_behave_request('GET', url)
 
 
-@given('I try to specify nonexistent exercise ID for view history')
+@given('I try to specify nonexistent exercise ID')
 def step_impl(context: Context):
-    url = f'{SERVER_API_V1_PATH}/exercise/nonexistent_id/history'
+    url = f'{SERVER_API_V1_PATH}/exercise/nonexistent_id/view'
     context.response = authorized_behave_request('GET', url)
--- rest/tests/behave/features/view_exercise.feature
+++ rest/tests/behave/features/view_exercise.feature
@@ -1,13 +1,13 @@
-Feature: View exercise history
+Feature: View exercise
 
   Scenario: Do it right
-      Given I try to get exercise history right
+      Given I try to view exercise right
        Then I will get Ok http status
 
   Scenario: Specify invalid ID
-      Given I try to specify invalid exercise ID for view history
+      Given I try to specify invalid exercise ID
        Then I will get "400" http error with "-32001" application error
 
   Scenario: Specify nonexistent ID
-      Given I try to specify nonexistent exercise ID for view history
+      Given I try to specify nonexistent exercise ID
        Then I will get "400" http error with "-32002" application error
